f14a48601bb253ecde4e98f7d2e67ab1a95c8e46
Sergey <smtishkar@mail.ru>
2023-02-06 19:12:06 +0300

    код работает с определением точки выхода. Осталось сделать подсветку кратчайшего маршрута и алгоритм моестить в метод

--- Lesson4.java
+++ Lesson4.java
@@ -14,7 +14,7 @@ public class Lesson4 {
         int m = 10; // количество столбцов
         int[][] arr = new int[n][m];
         int startpoint = 1;
-        // int endpoint = 99;      // надо будет потом поменять 99
+        int endpoint = 99;      // надо будет потом поменять 99
         int[] blocks = new int[] { 21, 30, 39, 48, 22, 23, 24 }; // Номера ячеек по порядку
 
         int[][] newarea = arrOneByOneFilling(arr, n, m);
@@ -36,29 +36,25 @@ public class Lesson4 {
         int j = 2;
         newarrWithBlocks[i][j] = startpoint;
 
-        // int o = 6;
-        // int p = 7;
+        int o = 6;
+        int p = 7;
         // newarrWithBlocks[o][p] = endpoint;
 
         queuey.add(i);
         queuex.add(j);
         print(newarrWithBlocks, n, m);
         System.out.println();
-        System.out.println(queuey);
-        System.out.println(queuex);
+        // System.out.println(queuey);
+        // System.out.println(queuex);
 
-        while (queuex.size() != 0 ) {
+        while (i != o || j != p ) {
             i = queuey.element();
             j = queuex.element();
             
             if (newarrWithBlocks[i - 1][j] == 0) {
                 newarrWithBlocks[i - 1][j] = newarrWithBlocks[i][j] + 1;
                 queuey.add(i-1);
-                queuex.add(j);
-                System.out.println(i);
-                System.out.println(j);
-                System.out.println(newarrWithBlocks[i][j]);
-                
+                queuex.add(j);                
             }
             if (newarrWithBlocks[i][j + 1] ==0) {
                 newarrWithBlocks[i][j + 1] = newarrWithBlocks[i][j] + 1;
@@ -77,14 +73,15 @@ public class Lesson4 {
             }
             queuex.remove();
             queuey.remove();
-            System.out.println(queuey);
-            System.out.println(queuex);
+            // System.out.println(queuey);
+            // System.out.println(queuex);
+            // System.out.println(i);
+            // System.out.println(j);
 
         }
+        newarrWithBlocks[o][p] = endpoint;
         print(newarrWithBlocks, n, m);
-        System.out.println(queuey);
-        System.out.println(queuex);
-        
+       
 
     }
 
