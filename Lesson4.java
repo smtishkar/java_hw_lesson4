561e21a261cbbb171a7847b86e69f898ba7909c9
Sergey <smtishkar@mail.ru>
2023-02-06 18:42:02 +0300

    код работает, но еще пока без точки выхода

--- Lesson4.java
+++ Lesson4.java
@@ -7,12 +7,14 @@ import java.util.Queue;
 public class Lesson4 {
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-
+        Queue<Integer> queuex = new LinkedList<Integer>();
+        Queue<Integer> queuey = new LinkedList<Integer>();
+    
         int n = 10; // количество строк
         int m = 10; // количество столбцов
         int[][] arr = new int[n][m];
         int startpoint = 1;
+        // int endpoint = 99;      // надо будет потом поменять 99
         int[] blocks = new int[] { 21, 30, 39, 48, 22, 23, 24 }; // Номера ячеек по порядку
 
         int[][] newarea = arrOneByOneFilling(arr, n, m);
@@ -26,36 +28,65 @@ public class Lesson4 {
         int[][] arrWithBlocks = blocksprep(arr, n, m, blocks);
         print(arrWithBlocks, n, m);
         System.out.println();
+
         int[][] newarrWithBlocks = blockspreptozero(arrWithBlocks, n, m);
         print(newarrWithBlocks, n, m);
         System.out.println();
-        newarrWithBlocks[5][2] = startpoint;
-        queue.add(newarrWithBlocks[5][2]);
+        int i = 5;
+        int j = 2;
+        newarrWithBlocks[i][j] = startpoint;
+
+        // int o = 6;
+        // int p = 7;
+        // newarrWithBlocks[o][p] = endpoint;
+
+        queuey.add(i);
+        queuex.add(j);
         print(newarrWithBlocks, n, m);
         System.out.println();
+        System.out.println(queuey);
+        System.out.println(queuex);
 
-        while (queue.size() > 0){
-            if (newarrWithBlocks[i-1][j] != -1 ){
-                newarrWithBlocks[i-1][j] = newarrWithBlocks[i][j]+1;
-                queue.add(newarrWithBlocks[i-1][j]);
-            }
-            if (newarrWithBlocks[i][j+1] != -1 ){
-                newarrWithBlocks[i][j+1] = newarrWithBlocks[i][j]+1;
-                queue.add(newarrWithBlocks[i][j+1]);     
-            }
-            if (newarrWithBlocks[i+1][j] != -1 ){
-                newarrWithBlocks[i+1][j] = newarrWithBlocks[i][j]+1;
-                queue.add(newarrWithBlocks[i+1][j]);
-            }
-            if (newarrWithBlocks[i][j-1] != -1 ){
-                newarrWithBlocks[i][j-1] = newarrWithBlocks[i][j]+1;
-                queue.add(newarrWithBlocks[i][j-1]);     
-            }
+        while (queuex.size() != 0 ) {
+            i = queuey.element();
+            j = queuex.element();
             
-        }print(newarrWithBlocks, n, m);
+            if (newarrWithBlocks[i - 1][j] == 0) {
+                newarrWithBlocks[i - 1][j] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i-1);
+                queuex.add(j);
+                System.out.println(i);
+                System.out.println(j);
+                System.out.println(newarrWithBlocks[i][j]);
+                
+            }
+            if (newarrWithBlocks[i][j + 1] ==0) {
+                newarrWithBlocks[i][j + 1] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i);
+                queuex.add(j+1);
+            }
+            if (newarrWithBlocks[i + 1][j] ==0) {
+                newarrWithBlocks[i + 1][j] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i+1);
+                queuex.add(j);
+            }
+            if (newarrWithBlocks[i][j - 1] ==0) {
+                newarrWithBlocks[i][j - 1] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i);
+                queuex.add(j-1);
+            }
+            queuex.remove();
+            queuey.remove();
+            System.out.println(queuey);
+            System.out.println(queuex);
+
+        }
+        print(newarrWithBlocks, n, m);
+        System.out.println(queuey);
+        System.out.println(queuex);
         
+
     }
-        
 
     public static void print(int arr[][], int n, int m) {
         for (int i = 1; i < n; i++) {
@@ -117,14 +148,5 @@ public class Lesson4 {
         return arr;
     }
 
-    // public static int[][] algorithm(int arr[][], int n, int m,) {
-    // for (int i = 1; i < n; i++) {
-    // for (int j = 1; j < m; j++) {
-    // if (arr[i-1][j] != -1){
-    // queue.add = arr[i-1][j];
-    // }
-    // }
-    // }
-    // return arr;
-    // }
+
 }
