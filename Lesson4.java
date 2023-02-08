7e57dd5d05cedada8c080bc7573d479e9af405b5
Sergey <smtishkar@mail.ru>
2023-02-08 10:42:46 +0300

    слегка подформатировал, начинаю переносить алгоритм в методы

--- Lesson4.java
+++ Lesson4.java
@@ -2,20 +2,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Lesson4
+ * Реализация волнового алгоритма (алгоритм Ли)
  */
 public class Lesson4 {
 
     public static void main(String[] args) {
         Queue<Integer> queuex = new LinkedList<Integer>();
         Queue<Integer> queuey = new LinkedList<Integer>();
-    
         int n = 20; // количество строк
         int m = 10; // количество столбцов
         int[][] arr = new int[n][m];
         int startpoint = 1;
-        int endpoint = 99;      // надо будет потом поменять 99
-        int[] blocks = new int[] { 21, 30, 39, 48, 57, 66, 75, 84, 85, 94, 103, 104, 113, 122, 22, 23, 24 }; // Номера ячеек по порядку
+        int endpoint = 99;
+        int[] blocks = new int[] { 21, 30, 39, 48, 57,
+                66, 75, 84, 85, 94, 103,
+                104, 113, 122, 22, 23, 24 }; // Номера ячеек по порядку где будут блоки
+        int i = 5; // Это номер строки где будет находиться входная точка
+        int j = 2; // Это номер столбца где будет находиться входная точка
+        int o = 14; // Это номер строки где будет находиться выходная точка
+        int p = 7; // Это номер столбца где будет находиться выходная точка
 
         int[][] newarea = arrOneByOneFilling(arr, n, m);
         print(arr, n, m);
@@ -32,43 +37,38 @@ public class Lesson4 {
         int[][] newarrWithBlocks = blockspreptozero(arrWithBlocks, n, m);
         print(newarrWithBlocks, n, m);
         System.out.println();
-        int i = 5;  //5
-        int j = 2;  //2
+
+        
         newarrWithBlocks[i][j] = startpoint;
-
-        int o = 6;
-        int p = 7;
-
 
         queuey.add(i);
         queuex.add(j);
         print(newarrWithBlocks, n, m);
         System.out.println();
 
-
-        while (i != o || j != p ) {
+        while (i != o || j != p) {
             i = queuey.element();
             j = queuex.element();
-            
+
             if (newarrWithBlocks[i - 1][j] == 0) {
                 newarrWithBlocks[i - 1][j] = newarrWithBlocks[i][j] + 1;
-                queuey.add(i-1);
-                queuex.add(j);                
-            }
-            if (newarrWithBlocks[i][j + 1] ==0) {
-                newarrWithBlocks[i][j + 1] = newarrWithBlocks[i][j] + 1;
-                queuey.add(i);
-                queuex.add(j+1);
-            }
-            if (newarrWithBlocks[i + 1][j] ==0) {
-                newarrWithBlocks[i + 1][j] = newarrWithBlocks[i][j] + 1;
-                queuey.add(i+1);
+                queuey.add(i - 1);
                 queuex.add(j);
             }
-            if (newarrWithBlocks[i][j - 1] ==0) {
+            if (newarrWithBlocks[i][j + 1] == 0) {
+                newarrWithBlocks[i][j + 1] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i);
+                queuex.add(j + 1);
+            }
+            if (newarrWithBlocks[i + 1][j] == 0) {
+                newarrWithBlocks[i + 1][j] = newarrWithBlocks[i][j] + 1;
+                queuey.add(i + 1);
+                queuex.add(j);
+            }
+            if (newarrWithBlocks[i][j - 1] == 0) {
                 newarrWithBlocks[i][j - 1] = newarrWithBlocks[i][j] + 1;
                 queuey.add(i);
-                queuex.add(j-1);
+                queuex.add(j - 1);
             }
             queuex.remove();
             queuey.remove();
@@ -76,7 +76,7 @@ public class Lesson4 {
         }
 
         print(newarrWithBlocks, n, m);
-       
+
         queuex.clear();
         queuey.clear();
 
@@ -87,38 +87,38 @@ public class Lesson4 {
         queuex.add(j);
         System.out.println();
 
-        while (queuex.size() > 0) {  
-                 
+        while (queuex.size() > 0) {
+
             i = queuey.element();
             j = queuex.element();
 
             if (newarrWithBlocks[i][j - 1] == newarrWithBlocks[i][j] - 1) {
                 newarrWithBlocks[i][j - 1] = newarrWithBlocks[i][j] - 1;
                 queuey.add(i);
-                queuex.add(j-1);  
-                newarrWithBlocks[i][j] = 99; 
-      
+                queuex.add(j - 1);
+                newarrWithBlocks[i][j] = 99;
+
             }
-            if (newarrWithBlocks[i+1][j] == newarrWithBlocks[i][j] - 1) {
-                newarrWithBlocks[i+1][j] = newarrWithBlocks[i][j] - 1;
-                queuey.add(i+1);
+            if (newarrWithBlocks[i + 1][j] == newarrWithBlocks[i][j] - 1) {
+                newarrWithBlocks[i + 1][j] = newarrWithBlocks[i][j] - 1;
+                queuey.add(i + 1);
                 queuex.add(j);
                 newarrWithBlocks[i][j] = 99;
-                
+
             }
-            if (newarrWithBlocks[i][j+1] == newarrWithBlocks[i][j] - 1) {
-                newarrWithBlocks[i][j+1] = newarrWithBlocks[i][j] - 1;
+            if (newarrWithBlocks[i][j + 1] == newarrWithBlocks[i][j] - 1) {
+                newarrWithBlocks[i][j + 1] = newarrWithBlocks[i][j] - 1;
                 queuey.add(i);
-                queuex.add(j+1);
+                queuex.add(j + 1);
                 newarrWithBlocks[i][j] = 99;
-                
+
             }
             if (newarrWithBlocks[i - 1][j] == newarrWithBlocks[i][j] - 1) {
                 newarrWithBlocks[i - 1][j] = newarrWithBlocks[i][j] - 1;
-                queuey.add(i-1);
+                queuey.add(i - 1);
                 queuex.add(j);
                 newarrWithBlocks[i][j] = 99;
-                
+
             }
             queuex.remove();
             queuey.remove();
@@ -126,17 +126,25 @@ public class Lesson4 {
         }
         newarrWithBlocks[o][p] = endpoint;
         newarrWithBlocks[i][j] = 99;
-        
+
         print(newarrWithBlocks, n, m);
-       
-
-
 
     }
 
+    public static int[][] arrOneByOneFilling(int arr[][], int n, int m) { // Метод заполняет массива цифрами по порядку,
+                                                                          // чтобы потом можно было указать где будут
+                                                                          // стоять блоки
+        int num = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                num += 1;
+                arr[i][j] = num;
+            }
+        }
+        return arr;
+    }
 
-
-    public static void print(int arr[][], int n, int m) {
+    public static void print(int arr[][], int n, int m) { // Метода который распечатывает массив
         for (int i = 1; i < n; i++) {
             System.out.println();
             for (int j = 1; j < m; j++) {
@@ -146,7 +154,8 @@ public class Lesson4 {
         }
     }
 
-    public static int[][] arreapreparatio(int arr[][], int n, int m) {
+    public static int[][] arreapreparatio(int arr[][], int n, int m) { // Метод который ограничевает поле и ставит по
+                                                                       // периметру -1
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (i == 1 || i == n - 1 || j == 1 || j == m - 1) {
@@ -157,7 +166,7 @@ public class Lesson4 {
         return arr;
     }
 
-    public static int[][] blocksprep(int arr[][], int n, int m, int arr2[]) {
+    public static int[][] blocksprep(int arr[][], int n, int m, int arr2[]) { // Метод устанавливает блоки
         int count = 0;
         while (count < arr2.length) {
             for (int i = 1; i < n; i++) {
@@ -173,7 +182,9 @@ public class Lesson4 {
         return arr;
     }
 
-    public static int[][] blockspreptozero(int arr[][], int n, int m) {
+    public static int[][] blockspreptozero(int arr[][], int n, int m) { // Метод после установки блоков заменяет все
+                                                                        // числа на 0, кроме -1, чтобы начать волновой
+                                                                        // алгоритм
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (arr[i][j] != -1) {
@@ -184,17 +195,4 @@ public class Lesson4 {
         }
         return arr;
     }
-
-    public static int[][] arrOneByOneFilling(int arr[][], int n, int m) {
-        int num = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                num += 1;
-                arr[i][j] = num;
-            }
-        }
-        return arr;
-    }
-
-
 }
